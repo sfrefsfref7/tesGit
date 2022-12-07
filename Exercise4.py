1ee2721cd8e788d9ab2a6733f64d99f8baa3e584
sfrefsfref7 <119869292+sfrefsfref7@users.noreply.github.com>
2022-12-07 14:52:53 +0700

    Exercise4_RATCHAPOL_LONGSUM

---
+++ Exercise4.py
@@ -0,0 +1,10 @@
+G = 95
+P = 98
+C = 65
+S = 46
+
+print ("--- Your Score ---")
+print("FoundationEnglish:",G)
+print("General Business",":",P)
+print("Introduction to Computer Systems",":",C)
+print("Computer Programming",":",S)
